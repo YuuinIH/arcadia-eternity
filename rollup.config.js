72762bfb625221fca65b6627ad4735afda2abeb1
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2025-04-20 23:37:00 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,13 @@ import nodeExternals from 'rollup-plugin-node-externals'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
+console.log('Rollup 配置加载成功！')
+console.log('当前插件列表:', [
+  nodeResolve(), 
+  nodeExternals(), 
+  typescript()
+].map(p => p.name))
+
 export default {
   input: 'bin/cli.ts',
   output: {
